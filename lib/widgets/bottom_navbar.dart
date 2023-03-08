9a95756a0789f6c0b693b12ae23511c9fffa24ef
marcoserenelli <marco.serenelli00@gmail.com>
2023-03-08 19:43:03 +0100

    Adjusted Bottom navbar

--- lib/widgets/bottom_navbar.dart
+++ lib/widgets/bottom_navbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction_list.dart';
 
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
@@ -8,9 +9,32 @@ class BottomNavBar extends StatefulWidget {
 }
 
 class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    TransactionWidget(),
+    Text(
+      'Index 1: Todo',
+      style: TextStyle(fontSize: 35),
+    ),
+    Text(
+      'Index 2: Todo',
+      style: TextStyle(fontSize: 35),
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(
@@ -26,6 +50,9 @@ class _BottomNavBarState extends State<BottomNavBar> {
             label: 'Settings',
           ),
         ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
       ),
     );
   }
